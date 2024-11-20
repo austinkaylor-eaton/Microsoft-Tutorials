a98c42cda1d6b67e7b1c33fd9552833de7e416fa
Austin Kaylor <AustinJKaylor@eaton.com>
2024-11-19 23:52:26 -0500

    Fixing the sleep command in SQLServer.sh

--- Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/SQLServer.sh
+++ Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/SQLServer.sh
@@ -4,7 +4,7 @@
 /opt/mssql/bin/sqlservr &
 
 # Wait for SQL Server to start
-sleep 30s
+sleep 30
 
 # Run the initialization script
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /usr/src/app/SampleDB.sql
