88be8a8e67a16dd9f048889b88e82d5567837b4f
Austin Kaylor <AustinJKaylor@eaton.com>
2024-11-27 14:17:19 -0500

    Adding a new SMS client to the Synchronous project

---
+++ Azure Communication SMS samples for dotNET/SynchronousConsoleApp/AzureCommunicationServiceSmsClient.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Communication.Sms;
+
+namespace SynchronousConsoleClient;
+
+/// <summary>
+/// A client for sending SMS messages using Azure Communication Services
+/// </summary>
+public class AzureCommunicationServiceSmsClient(AzureCommunicationServiceOptions azureCommunicationServiceOptions)
+{
+    private readonly SmsClient _client = new(azureCommunicationServiceOptions.PrimaryConnectionString);
+
+    /// <summary>
+    /// Sends an SMS message synchronously to another US-based phone number
+    /// </summary>
+    /// <param name="phoneNumber">The US-based phone number to send the SMS message to</param>
+    public void SendMessageToPhoneNumber(string phoneNumber)
+    {
+        try
+        {
+            SmsSendResult sendResult = _client.Send(
+                from: azureCommunicationServiceOptions.PhoneNumber, // Your E.164 formatted from phone number used to send SMS
+                to: phoneNumber, // E.164 formatted recipient phone number
+                message: "Hi");
+            Console.WriteLine($"Sms id: {sendResult.MessageId}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
