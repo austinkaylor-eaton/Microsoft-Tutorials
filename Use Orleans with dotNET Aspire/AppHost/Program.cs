9ecc6b0c3b4b198f153f14e4096666e9919458f2
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-04 16:56:06 -0500

    Adding orleans to the AppHost project's Program.cs file

--- Use Orleans with dotNET Aspire/AppHost/Program.cs
+++ Use Orleans with dotNET Aspire/AppHost/Program.cs
@@ -1,3 +1,7 @@
+using Aspire.Hosting.Orleans;
+
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+OrleansService orleans = builder.AddOrleans("default"); // Add Orleans service -> name for diagnostic purposes
 
 builder.Build().Run();
