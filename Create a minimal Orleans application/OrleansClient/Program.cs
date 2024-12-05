3d05659d87185921124c1d8aa7e38d06ea5195a8
Austin Kaylor <148803110+austinkaylor-eaton@users.noreply.github.com>
2024-12-05 16:52:31 -0500

    Tutorial/create a minimal orleans application (#2)
    * Creating a new solution for the tutorial
    
    * Adding a README for the tutorial and adding it back to the Solution README.md
    
    * Adding some pre-requisites to the tutorial's README.md
    
    * Adding projects
    
    * Adding stuff about the projects and tutorial objects to the tutorial README.md
    
    * Installing Orleans NuGet packages
    
    * adding an interface for a new grain
    
    * Adding a concrete implementation of the IHelloGrain interface
    
    * Adding code to the OrleansSilo to configure it as a local cluster and single silo
    
    * Adding code for the client to interact with the silo
    
    * Adding local debugging instructions to the README.md

---
+++ Create a minimal Orleans application/OrleansClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrleansGrainInterfaces;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleansClient(client =>
+    {
+        client.UseLocalhostClustering();
+    })
+    .ConfigureLogging(logging => logging.AddConsole())
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+await host.StartAsync();
+
+IClusterClient client = host.Services.GetRequiredService<IClusterClient>();
+
+IHelloGrain friend = client.GetGrain<IHelloGrain>(0);
+string response = await friend.SayHello("Hi friend!");
+
+Console.WriteLine($"{response}\n\nPress any key to exit...");
+
+Console.ReadKey();
+
+await host.StopAsync();
---
+++ Create a minimal Orleans application/OrleansGrainInterfaces/IHelloGrain.cs
@@ -0,0 +1,8 @@
+﻿namespace OrleansGrainInterfaces;
+
+[Alias("OrleansGrainInterfaces.IHelloGrain")]
+public interface IHelloGrain : IGrainWithIntegerKey
+{
+    [Alias("SayHello")]
+    ValueTask<string> SayHello(string greeting);
+}
