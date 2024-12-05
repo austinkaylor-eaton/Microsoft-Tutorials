31722a49d6f84d8b67cfdee7cdefb03c2985f0d3
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 12:29:14 -0500

    Committing what I have

--- Use Orleans with dotNET Aspire/OrleansServer/Program.cs
+++ Use Orleans with dotNET Aspire/OrleansServer/Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Orleans.Runtime;
+using ServiceDefaults;
 
-app.MapGet("/", () => "Hello World!");
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+builder.AddServiceDefaults();
+builder.AddKeyedAzureTableClient("clustering");
+builder.AddKeyedAzureBlobClient("grain-state");
+builder.UseOrleans();
+
+WebApplication app = builder.Build();
+
+app.MapGet("/", () => "OK");
+
+await app.RunAsync();
+
+public sealed class CounterGrain(
+    [PersistentState("count")] IPersistentState<int> count) : ICounterGrain
+{
+    public ValueTask<int> Get()
+    {
+        return ValueTask.FromResult(count.State);
+    }
+
+    public async ValueTask<int> Increment()
+    {
+        int result = ++count.State;
+        await count.WriteStateAsync();
+        return result;
+    }
+}
