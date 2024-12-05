57c825da5d028a1128cdaf3f5ff84f43aded5b68
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 16:20:32 -0500

    Adding code to the OrleansSilo to configure it as a local cluster and single silo

--- Create a minimal Orleans application/OrleansSilo/Program.cs
+++ Create a minimal Orleans application/OrleansSilo/Program.cs
@@ -1,3 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering()
+            .ConfigureLogging(logging => logging.AddConsole());
+    })
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
