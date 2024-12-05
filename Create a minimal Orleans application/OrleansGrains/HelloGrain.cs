05c4030e8f617bc0a64fdb93cbb3449036e6020e
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 16:16:24 -0500

    Adding a concrete implementation of the IHelloGrain interface

---
+++ Create a minimal Orleans application/OrleansGrains/HelloGrain.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using OrleansGrainInterfaces;
+
+namespace OrleansGrains;
+
+public class HelloGrain(ILogger<HelloGrain> logger) : Grain, IHelloGrain
+{
+    private readonly ILogger _logger = logger;
+
+    ValueTask<string> IHelloGrain.SayHello(string greeting)
+    {
+        _logger.LogInformation("SayHello message received: greeting = \"{Greeting}\"",
+            greeting);
+        
+        return ValueTask.FromResult($"\nClient said: \"{greeting}\", so HelloGrain says: Hello!");
+    }
+}
