69beff484c491e5797f837d58b033717e2867dab
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 12:10:28 -0500

    Adding an OrleansClient and OrleansServer project and adding them as references to the AppHost project

--- Use Orleans with dotNET Aspire/AppHost/Program.cs
+++ Use Orleans with dotNET Aspire/AppHost/Program.cs
@@ -2,6 +2,34 @@ using Aspire.Hosting.Orleans;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-OrleansService orleans = builder.AddOrleans("default"); // Add Orleans service -> name for diagnostic purposes
+// Add the resources which you will use for Orleans clustering and
+// grain state storage.
+var storage = builder.AddAzureStorage("storage").RunAsEmulator();
+var clusteringTable = storage.AddTables("clustering");
+var grainStorage = storage.AddBlobs("grain-state");
 
-builder.Build().Run();
+// Add the Orleans resource to the Aspire DistributedApplication
+// builder, then configure it with Azure Table Storage for clustering
+// and Azure Blob Storage for grain storage.
+OrleansService? orleans = builder.AddOrleans("default")  // Add Orleans service -> name for diagnostic purposes
+    .WithClustering(clusteringTable)
+    .WithGrainStorage("Default", grainStorage);
+
+// Add our server project and reference your 'orleans' resource from it.
+// it can join the Orleans cluster as a service.
+// This implicitly add references to the required resources.
+// In this case, that is the 'clusteringTable' resource declared earlier.
+builder.AddProject<Projects.OrleansServer>("silo")
+    .WithReference(orleans)
+    .WithReplicas(3);
+
+// Reference the Orleans resource as a client from the 'frontend'
+// project so that it can connect to the Orleans cluster.
+builder.AddProject<Projects.OrleansClient>("frontend")
+    .WithReference(orleans.AsClient())
+    .WithExternalHttpEndpoints()
+    .WithReplicas(3);
+
+// Build and run the application.
+await using DistributedApplication app = builder.Build();
+await app.RunAsync();
