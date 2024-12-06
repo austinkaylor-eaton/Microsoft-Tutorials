f1b1544dd02de66fd41c838a4c56ecbe1e039d89
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-06 10:22:17 -0500

    Adding SQL Server to AppHost's Program.cs

--- Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
+++ Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
@@ -2,9 +2,16 @@ IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(ar
 
 IResourceBuilder<RedisResource> cache = builder.AddRedis("cache");
 
-builder.AddProject<Projects.API>("api")
-    .WithReference(cache);
+IResourceBuilder<SqlServerServerResource> sql = builder.AddSqlServer("sql")
+    .WithLifetime(ContainerLifetime.Persistent);
 
+IResourceBuilder<SqlServerDatabaseResource> db = sql.AddDatabase("database");
+
+builder.AddProject<Projects.API>("api")
+    .WithReference(cache)
+    .WithReference(db)
+    .WaitFor(db);
+   
 // After adding all resources, run the app...
 
 builder.Build().Run();
