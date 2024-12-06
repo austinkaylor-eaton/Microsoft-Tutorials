cc8db48d2118d096c41b7690a5b5e9efb112a114
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 19:08:49 -0500

    splitting out the UrlDetails sealed record into it's own file

---
+++ Build your first Orleans app with ASPNET Core/OrleansURLShortener/UrlDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace OrleansURLShortener;
+
+[GenerateSerializer, Alias(nameof(UrlDetails))]
+public sealed record class UrlDetails
+{
+    [Id(0)]
+    public string FullUrl { get; set; } = "";
+
+    [Id(1)]
+    public string ShortenedRouteSegment { get; set; } = "";
+}
--- Build your first Orleans app with ASPNET Core/OrleansURLShortener/UrlShortenerGrain.cs
+++ Build your first Orleans app with ASPNET Core/OrleansURLShortener/UrlShortenerGrain.cs
@@ -21,13 +21,3 @@ public sealed class UrlShortenerGrain(
     public Task<string> GetUrl() =>
         Task.FromResult(state.State.FullUrl);
 }
-
-[GenerateSerializer, Alias(nameof(UrlDetails))]
-public sealed record class UrlDetails
-{
-    [Id(0)]
-    public string FullUrl { get; set; } = "";
-
-    [Id(1)]
-    public string ShortenedRouteSegment { get; set; } = "";
-}
