0b5ed0f3da05bb42b0867e81a519d396ccd1fd2d
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 20:38:44 -0500

    Adding Aspire AppHost and ServiceDefault projects

--- Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/WebApi/Program.cs
+++ Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/WebApi/Program.cs
@@ -1,10 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,7 +21,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
+        WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
                 (
                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
