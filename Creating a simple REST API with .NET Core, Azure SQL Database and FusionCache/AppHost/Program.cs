7379045bd1c68f5fe423a93b8ac3f7449c296da9
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-06 10:14:54 -0500

    Adding Redis to the AppHost

--- Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
+++ Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
@@ -1,3 +1,10 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
+IResourceBuilder<RedisResource> cache = builder.AddRedis("cache");
+
+builder.AddProject<Projects.API>("api")
+    .WithReference(cache);
+
+// After adding all resources, run the app...
+
 builder.Build().Run();
