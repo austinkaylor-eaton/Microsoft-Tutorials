3a62e7b745bcb83c49013d284286cf962fbfa2c7
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 19:06:41 -0500

    Adding endpoints to shorten and get shortened urls

--- Build your first Orleans app with ASPNET Core/OrleansURLShortener/Program.cs
+++ Build your first Orleans app with ASPNET Core/OrleansURLShortener/Program.cs
@@ -1,4 +1,5 @@
 using Orleans.Runtime;
+using OrleansURLShortener;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -9,3 +10,55 @@ builder.Host.UseOrleans(static siloBuilder =>
 });
 
 using WebApplication app = builder.Build();
+
+app.MapGet("/", static () => "Welcome to the URL shortener, powered by Orleans!");
+
+app.MapGet("/shorten",
+    static async (IGrainFactory grains, HttpRequest request, string url) =>
+    {
+        var host = $"{request.Scheme}://{request.Host.Value}";
+
+        // Validate the URL query string.
+        if (string.IsNullOrWhiteSpace(url) ||
+            Uri.IsWellFormedUriString(url, UriKind.Absolute) is false)
+        {
+            return Results.BadRequest($"""
+                                       The URL query string is required and needs to be well formed.
+                                       Consider, ${host}/shorten?url=https://www.microsoft.com.
+                                       """);
+        }
+
+        // Create a unique, short ID
+        var shortenedRouteSegment = Guid.NewGuid().GetHashCode().ToString("X");
+
+        // Create and persist a grain with the shortened ID and full URL
+        IUrlShortenerGrain? shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        await shortenerGrain.SetUrl(url);
+
+        // Return the shortened URL for later use
+        UriBuilder resultBuilder = new UriBuilder(host)
+        {
+            Path = $"/go/{shortenedRouteSegment}"
+        };
+
+        return Results.Ok(resultBuilder.Uri);
+    });
+
+app.MapGet("/go/{shortenedRouteSegment:required}",
+    static async (IGrainFactory grains, string shortenedRouteSegment) =>
+    {
+        // Retrieve the grain using the shortened ID and url to the original URL
+        IUrlShortenerGrain? shortenerGrain =
+            grains.GetGrain<IUrlShortenerGrain>(shortenedRouteSegment);
+
+        string url = await shortenerGrain.GetUrl();
+
+        // Handles missing schemes, defaults to "http://".
+        UriBuilder redirectBuilder = new UriBuilder(url);
+
+        return Results.Redirect(redirectBuilder.Uri.ToString());
+    });
+
+app.Run();
