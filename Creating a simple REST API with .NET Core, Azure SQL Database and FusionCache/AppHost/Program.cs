8a83dad1b5143317328026f15b2399118a9970a7
Austin Kaylor <AustinJKaylor@eaton.com>
2024-12-05 20:58:03 -0500

    Adding sql server configuration shell files

--- Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
+++ Creating a simple REST API with .NET Core, Azure SQL Database and FusionCache/AppHost/Program.cs
@@ -1,3 +1,24 @@
-var builder = DistributedApplication.CreateBuilder(args);
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+// SQL Server container is configured with an auto-generated password by default
+// but doesn't support any auto-creation of databases or running scripts on startup so we have to do it manually.
+var sqlserver = builder.AddSqlServer("sqlserver")
+    // Mount the init scripts directory into the container.
+    .WithBindMount("./sqlserverconfig", "/usr/config")
+    // Mount the SQL scripts directory into the container so that the init scripts run.
+    .WithBindMount("../DatabaseContainers.ApiService/data/sqlserver", "/docker-entrypoint-initdb.d")
+    // Run the custom entrypoint script on startup.
+    .WithEntrypoint("/usr/config/entrypoint.sh")
+    // Configure the container to store data in a volume so that it persists across instances.
+    .WithDataVolume()
+    // Keep the container running between app host sessions.
+    .WithLifetime(ContainerLifetime.Persistent);
+
+// Add the database to the application model so that it can be referenced by other resources.
+IResourceBuilder<SqlServerDatabaseResource> database = sqlserver.AddDatabase("SampleDB");
+
+builder.AddProject<Projects.WebApi>("apiservice")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
